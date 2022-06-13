2c28374de943969c1b3cf4cac9527c7e235d7848
bysxx <0415white@gmail.com>
2022-06-13 15:06:16 +0900

    [ADD] styled-components setting update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
 import type { NextPage } from 'next'
 import Example from '../components/test';
 import useExample from '../hooks/useExample'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Home: NextPage = () => {
   const { data, setData } = useExample();
 
   return (
-    <div>
+    <Container>
       <Example />
-    </div>
+    </Container>
   )
 }
 
