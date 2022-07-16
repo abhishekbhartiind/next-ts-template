325df38454f7fbf4bec25b19c0237a4f6afeddd2
bysxx <0415white@gmail.com>
2022-07-16 18:50:50 +0900

    [FIX] axios manager post request 수정

--- libs/axiosManager.ts
+++ libs/axiosManager.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://localhost:5000';
 
 export const getRequest = async <T>(url: string, query?: string, config?: any): Promise<T> => {
   const reqQuery = `${query && query.length > 0 ? `?${query}` : ''}`;
@@ -12,15 +12,10 @@ export const getRequest = async <T>(url: string, query?: string, config?: any): 
   return result;
 };
 
-export const postRequest = async <T>(url: string, body: any, config: any, onError?: (e: any) => void) => {
-  const result = axios
-    .post<T>(url, body, config)
-    .then((resp) => {
-      return resp;
-    })
-    .catch((e) => {
-      onError && onError(e);
-    });
+export const postRequest = async <T>(url: string, body: any, config?: any, onError?: (e: any) => void): Promise<T> => {
+  const result = axios.post<T>(baseUrl + url, body, config).then((resp) => {
+    return resp.data;
+  });
 
   return result;
 };
