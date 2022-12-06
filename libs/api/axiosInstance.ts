2e146f956a205bf1a0b02a2ad28ba8692d457193
yoonsung baek <0415white@gmail.com>
2022-12-07 08:19:19 +0900

    [ENH] enhance axios instance

--- libs/api/axiosInstance.ts
+++ libs/api/axiosInstance.ts
@@ -2,4 +2,19 @@ import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create();
 
+axiosInstance.interceptors.response.use(
+  res => {
+    return res
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
+axiosInstance.interceptors.request.use(
+  req => {
+    return req
+  }
+)
+
 export default axiosInstance;
--- libs/api/example.ts
+++ libs/api/example.ts
@@ -1,4 +1,3 @@
 import axiosInstance from '@libs/api/axiosInstance';
-import { IExample } from '@interfaces/index';
 
 export const getExampleData = () => axiosInstance.get<any>('https://api.coinpaprika.com/v1/ticker/cspr-casper-network').then((res) => res.data);
