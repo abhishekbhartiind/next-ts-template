b9a4da2d7ab9ce76e472ce873c1784b9c1dcab70
yoonsung baek <0415white@gmail.com>
2022-12-12 21:21:30 +0900

    [ENH] delete prev axios and update folder path

--- libs/api/example.ts
+++ libs/api/example.ts
@@ -1,3 +1,3 @@
-import axiosInstance from '@libs/api/axiosInstance';
+import axiosInstance from '@libs/axiosInstance';
 
 export const getExampleData = () => axiosInstance.get<any>('https://api.coinpaprika.com/v1/ticker/cspr-casper-network').then((res) => res.data);
