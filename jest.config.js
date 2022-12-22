567ac37bb0c5af71af69ff836686b5203ee2a8ba
yoonsung baek <0415white@gmail.com>
2022-12-22 17:02:09 +0900

    [ENH] jest update

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+const customJestConfig = {
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@(pages|components|hooks|interfaces|libs|layouts|styles)/(.+)$": "<rootDir>/$1/$2",
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@/public/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 30,
+      functions: 30,
+      lines: 30,
+      statements: 30,
+    },
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
---
+++ test/pages/index.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+
+import Index from '../../pages/index';
+import Wrapper from '../TestWrapper';
+
+describe('Index page', () => {
+  describe('Render method', () => {
+    it('should have h1 tag', () => {
+      render(
+        <Wrapper>
+          <Index />
+        </Wrapper>
+      );
+
+      const heading = screen.getByRole('heading', {
+        name: /main/,
+      });
+
+      expect(heading).toBeInTheDocument();
+    });
+  });
+});
